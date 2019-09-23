704de1424f99b26219dbfeb1ec006f496f9061b6
András Ács <acsandras@gmail.com>
2019-09-23 11:33:20 +0200

    Forbedringer :)

--- src/Bmi.java
+++ src/Bmi.java
@@ -7,12 +7,26 @@
 
 public class Bmi {
 
+
+    /** Denne metode returnerer BMI for indtastet højde og vægt
+     *  -- Vægt skal være i kg
+     *  -- Højde skal være i meter
+     *  -- Hvis ugyldige parametre indtastes, så vil denne metode returne -1
+     */
     static double bmiBeregning(double hojde, double vaegt) {
+
+        final double MIN_HOJDE = 0.01;
+        final double MAX_HOJDE = 3.0;
 
         // Resultat med mange decimaler
         double resultat = vaegt / Math.pow(hojde, 2);
-        // Resultat med 2 decimaler
+        // Vi runder resultatet ned til 2 decimaler (tak, Emil)
         resultat = Math.round(resultat*10.0)/10.0;
+
+        if (hojde>MAX_HOJDE || hojde<MIN_HOJDE ) {
+            return -1;
+        }
+
         return resultat;
     }
 
--- test/BmiTest.java
+++ test/BmiTest.java
@@ -1,12 +1,33 @@
 import junit.framework.TestCase;
 
+
 public class BmiTest extends TestCase {
 
-
+    /** Tester András vægt*/
     public void testBmiBeregning() {
-        // tage imod højde i meter og vægt kg
         double resultat;
         resultat = Bmi.bmiBeregning(1.85, 95);
         assertEquals(27.8, resultat);
     }
+    /** Tester maximum højde */
+    public void testBmiBeregningMaximum() {
+        double resultat;
+        resultat = Bmi.bmiBeregning(3.01, 95);
+        assertEquals(-1.0, resultat);
+    }
+
+    /** Tester minimum højde */
+    public void testBmiBeregningMinimum() {
+        double resultat;
+        resultat = Bmi.bmiBeregning(0.001, 95);
+        assertEquals(-1.0, resultat);
+    }
+
+    /** Tester 0 som højde */
+    public void testBmiBeregningDivisionBy0() {
+        double resultat;
+        resultat = Bmi.bmiBeregning(0.0, 95);
+        assertEquals(-1.0, resultat);
+    }
+
 }
